888e7cc7b71257366c0b193c86a0f08b1350ff32
fatimaazdzikrun <65176548+fatimaazdzikrun@users.noreply.github.com>
2022-06-23 12:00:53 +0800

    Add files via upload

---
+++ Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DHLSystem1.Models
+{
+    public class UserModel
+    {
+       
+            public int Id { get; set; }
+            public string username { get; set; }
+            public string password { get; set; }
+        
+    }
+}
